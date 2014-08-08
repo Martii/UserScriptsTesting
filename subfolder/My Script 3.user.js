a86cdc6a83cc5880da5a901c9562429425463995
Martii <thalamew@q.com>
2014-08-07 20:56:32 -0600

    My Script 3 test

---
+++ subfolder/My Script 3.user.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+// ==UserScript==
+// @namespace    http://localhost.localdomain
+
+// @name         My Script 3
+// @name:fr-FR   Mon Script 3
+// @name:ru-RU   Мой сценарий 3
+
+// @description        It even does the laundry!
+// @description:fr-FR  Il a même fait la lessive !
+// @description:ru-RU  Он даже делает стирку!
+
+// @version      0.0.0
+
+// @include      http://www.example.com
+// @include      http://www.example.org
+// @include      http://www.example.net
+
+// @author Marti Martz (https://github.com/marti)
+
+// @icon https://gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?s=48
+
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author marti
+// @collaborator sizzle
+// ==/OpenUserJS==
+
+})();
